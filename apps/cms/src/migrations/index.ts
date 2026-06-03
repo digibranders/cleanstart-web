f10b710905d73d304749edf40581fc41f7aafcb5
Gaurav <admin@digibrander.com>
2026-06-03 13:48:55 +0530

    feat(cms): migration for careers collections

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260602_050520_initial_baseline from './20260602_050520_initial_baseline';
 import * as migration_20260602_095540_remove_brevo from './20260602_095540_remove_brevo';
 import * as migration_20260602_115320_add_hubspot_form_guid from './20260602_115320_add_hubspot_form_guid';
+import * as migration_20260603_081714_add_careers_collections from './20260603_081714_add_careers_collections';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260602_115320_add_hubspot_form_guid.up,
     down: migration_20260602_115320_add_hubspot_form_guid.down,
-    name: '20260602_115320_add_hubspot_form_guid'
+    name: '20260602_115320_add_hubspot_form_guid',
+  },
+  {
+    up: migration_20260603_081714_add_careers_collections.up,
+    down: migration_20260603_081714_add_careers_collections.down,
+    name: '20260603_081714_add_careers_collections'
   },
 ];
